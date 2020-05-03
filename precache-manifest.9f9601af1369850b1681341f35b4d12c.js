45115befa06e76f70daf31c0827cd416fe1e944c
Joy Ghosh <joyghosh826@gmail.com>
2020-05-02 23:22:37 -0600

    Updates

---
+++ precache-manifest.9f9601af1369850b1681341f35b4d12c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046b73d03bd5d58ff1479136a0a05fda",
+    "url": "/TaxChainLive/index.html"
+  },
+  {
+    "revision": "a99846f7da949f0bea9b",
+    "url": "/TaxChainLive/static/css/2.fc9e7abf.chunk.css"
+  },
+  {
+    "revision": "5c320b6a241e895c06e6",
+    "url": "/TaxChainLive/static/css/main.c910600f.chunk.css"
+  },
+  {
+    "revision": "a99846f7da949f0bea9b",
+    "url": "/TaxChainLive/static/js/2.dfed116e.chunk.js"
+  },
+  {
+    "revision": "e97891d6fd84a0c031ad02ab85f865e9",
+    "url": "/TaxChainLive/static/js/2.dfed116e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c320b6a241e895c06e6",
+    "url": "/TaxChainLive/static/js/main.b4f0ba76.chunk.js"
+  },
+  {
+    "revision": "c60f053cda872fdf1781",
+    "url": "/TaxChainLive/static/js/runtime-main.e51f7383.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/TaxChainLive/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/TaxChainLive/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/TaxChainLive/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/TaxChainLive/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/TaxChainLive/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/TaxChainLive/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/TaxChainLive/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/TaxChainLive/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/TaxChainLive/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/TaxChainLive/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/TaxChainLive/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/TaxChainLive/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/TaxChainLive/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/TaxChainLive/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/TaxChainLive/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/TaxChainLive/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
